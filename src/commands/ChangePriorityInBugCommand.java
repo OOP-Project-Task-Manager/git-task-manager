0deee86348b81b290faead2f4ae88bbf76786563
Stefcho1227 <stefan.ivanov1227@gmail.com>
2024-05-16 23:25:35 +0300

    implemented ChangePriorityInBugCommand class

--- src/commands/ChangePriorityInBugCommand.java
+++ src/commands/ChangePriorityInBugCommand.java
@@ -10,6 +10,11 @@ import utilities.ValidationHelper;
 import java.util.List;
 
 public class ChangePriorityInBugCommand extends BaseCommand {
+    public static final int EXPECTED_ARGUMENTS_COUNT = 2;
+    public static final String NOT_A_VALID_INTEGER = "Not a valid integer";
+    public static final String PRIORITY_ERR = "Priority {%s} does not exist";
+    public static final String PRIORITY_CHANGE = "Priority of task %s changed to %s";
+
 
     protected ChangePriorityInBugCommand(TaskRepository repository) {
         super(repository);
@@ -17,8 +22,17 @@ public class ChangePriorityInBugCommand extends BaseCommand {
 
     @Override
     protected String executeCommand(List<String> parameters) {
-        return null;
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_ARGUMENTS_COUNT);
+        int id = ParsingHelpers.tryParseInteger(parameters.get(0), NOT_A_VALID_INTEGER);
+        Priority priority = ParsingHelpers.tryParseEnum(parameters.get(1),
+                Priority.class,
+                PRIORITY_ERR.formatted(parameters.get(1)));
+        Bug bug = getRepository().findBugById(id);
+        return changePriority(bug, priority);
     }
-
+    private String changePriority(Bug bug, Priority priority) {
+        bug.setPriority(priority);
+        return String.format(PRIORITY_CHANGE, bug.getTitle(), priority);
+    }
 
 }
