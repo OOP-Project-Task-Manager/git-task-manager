737a2f33d2450a7f4d0241a81f0dbae0a174b4e3
Dimitar Hadzhiev <hadzhiev96@gmail.com>
2024-05-17 00:33:45 +0300

    ChangeStatusInBugCommand implemented

---
+++ src/commands/ChangeStatusInBugCommand.java
@@ -0,0 +1,36 @@
+package commands;
+
+import core.contracts.TaskRepository;
+import models.tasks.Contracts.Bug;
+import models.tasks.enums.StatusBug;
+import utilities.ParsingHelpers;
+import utilities.ValidationHelper;
+
+import java.util.List;
+
+public class ChangeStatusInBugCommand extends BaseCommand {
+    public static final int EXPECTED_ARGUMENTS_COUNT = 2;
+    public static final String STATUS_ERR = "Status {%s} does not exist";
+    public static final String NOT_A_VALID_INTEGER = "Not a valid integer";
+
+    public static final String STATUS_CHANGE = "Status of task %s changed to %s";
+
+    protected ChangeStatusInBugCommand(TaskRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    protected String executeCommand(List<String> parameters) {
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_ARGUMENTS_COUNT);
+        int id = ParsingHelpers.tryParseInteger(parameters.get(0), NOT_A_VALID_INTEGER);
+        StatusBug status = ParsingHelpers.tryParseEnum(parameters.get(1), StatusBug.class, STATUS_ERR);
+        Bug bug = getRepository().findBugById(id);
+
+        return changeStatus(bug, status);
+    }
+
+    private String changeStatus(Bug bug, StatusBug status) {
+        bug.setStatus(status);
+        return String.format(STATUS_CHANGE, bug.getTitle(), status);
+    }
+}
