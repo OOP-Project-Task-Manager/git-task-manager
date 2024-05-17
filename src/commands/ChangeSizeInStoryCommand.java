da5d3e5a9cb829e7aee7d09087a55a175a0e921c
Stefcho1227 <stefan.ivanov1227@gmail.com>
2024-05-17 10:43:05 +0300

    implemented ChangeSizeInStoryCommand class and ChangeStatusInStoryCommand

--- src/commands/ChangeSizeInStoryCommand.java
+++ src/commands/ChangeSizeInStoryCommand.java
@@ -1,4 +1,37 @@
 package commands;
 
-public class ChangeSizeInStoryCommand {
+import core.contracts.TaskRepository;
+import models.tasks.Contracts.Story;
+import models.tasks.enums.Size;
+import utilities.ParsingHelpers;
+import utilities.ValidationHelper;
+
+import java.util.List;
+import java.util.Stack;
+
+public class ChangeSizeInStoryCommand extends BaseCommand {
+
+    public static final int EXPECTED_ARGUMENTS_COUNT = 2;
+    public static final String SIZE_ERR = "Size {%s} does not exist";
+    public static final String NOT_A_VALID_INTEGER = "Not a valid integer";
+
+    public static final String SIZE_CHANGE = "Size of task %s changed to %s";
+
+
+    protected ChangeSizeInStoryCommand(TaskRepository repository){
+        super(repository);
+    }
+    @Override
+    protected String executeCommand(List<String> parameters) {
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_ARGUMENTS_COUNT);
+        int id = ParsingHelpers.tryParseInteger(parameters.get(0), NOT_A_VALID_INTEGER);
+        Size size = ParsingHelpers.tryParseEnum(parameters.get(1), Size.class, SIZE_ERR);
+        Story story = getRepository().findStoryById(id);
+        return changeSizeInStory(story, size);
+    }
+
+    private String changeSizeInStory(Story story, Size size){
+        story.setSize(size);
+        return String.format(SIZE_CHANGE, story.getTitle(), size);
+    }
 }
--- src/commands/ChangeStatusInStoryCommand.java
+++ src/commands/ChangeStatusInStoryCommand.java
@@ -1,4 +1,34 @@
 package commands;
 
-public class ChangeStatusInStoryCommand {
+import core.contracts.TaskRepository;
+import models.tasks.Contracts.Story;
+import models.tasks.enums.StatusStory;
+import utilities.ParsingHelpers;
+import utilities.ValidationHelper;
+
+import java.util.List;
+
+public class ChangeStatusInStoryCommand extends BaseCommand {
+    public static final int EXPECTED_ARGUMENTS_COUNT = 2;
+    public static final String STATUS_ERR = "Status {%s} does not exist";
+    public static final String NOT_A_VALID_INTEGER = "Not a valid integer";
+
+    public static final String STATUS_CHANGE = "Status of task %s changed to %s";
+
+    protected ChangeStatusInStoryCommand(TaskRepository repository){
+        super(repository);
+    }
+    @Override
+    protected String executeCommand(List<String> parameters) {
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_ARGUMENTS_COUNT);
+        int id = ParsingHelpers.tryParseInteger(parameters.get(0), NOT_A_VALID_INTEGER);
+        StatusStory status = ParsingHelpers.tryParseEnum(parameters.get(1), StatusStory.class ,STATUS_ERR);
+        Story story = getRepository().findStoryById(id);
+        return changeStatusInStory(story, status);
+    }
+
+    private String changeStatusInStory(Story story, StatusStory status){
+        story.setStatus(status);
+        return String.format(STATUS_CHANGE, story.getTitle(), status);
+    }
 }
