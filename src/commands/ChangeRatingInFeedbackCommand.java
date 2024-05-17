fd652c15fde79eb967fae7571c19a208a835efce
Dimitar Hadzhiev <hadzhiev96@gmail.com>
2024-05-17 12:19:59 +0300

    ChangeStatusInFeedbackCommand implemented

---
+++ src/commands/ChangeRatingInFeedbackCommand.java
@@ -0,0 +1,36 @@
+package commands;
+
+import core.contracts.TaskRepository;
+import models.tasks.Contracts.Feedback;
+import utilities.ParsingHelpers;
+import utilities.ValidationHelper;
+
+import java.util.List;
+
+public class ChangeRatingInFeedbackCommand extends BaseCommand {
+
+    public static final int EXPECTED_ARGUMENTS_COUNT = 2;
+    public static final String NOT_A_VALID_INTEGER = "Not a valid integer";
+    public static final String RATING_CHANGE = "Rating of task %s changed to %s";
+
+    protected ChangeRatingInFeedbackCommand(TaskRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    protected String executeCommand(List<String> parameters) {
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_ARGUMENTS_COUNT);
+        int id = ParsingHelpers.tryParseInteger(parameters.get(0), NOT_A_VALID_INTEGER);
+        Feedback feedback = getRepository().findFeedbackById(id);
+        int rating = ParsingHelpers.tryParseInteger(parameters.get(1), NOT_A_VALID_INTEGER);
+
+        return changeRating(feedback, rating);
+    }
+
+    private String changeRating(Feedback feedback, int rating) {
+        feedback.setRating(rating);
+        return String.format(RATING_CHANGE, feedback.getId(), rating);
+    }
+
+
+}
---
+++ src/commands/ChangeStatusInFeedbackCommand.java
@@ -0,0 +1,41 @@
+package commands;
+
+import core.contracts.TaskRepository;
+import models.tasks.Contracts.Feedback;
+import models.tasks.enums.StatusFeedback;
+import utilities.ParsingHelpers;
+import utilities.ValidationHelper;
+
+import java.util.List;
+
+public class ChangeStatusInFeedbackCommand extends BaseCommand {
+
+    public static final int EXPECTED_ARGUMENTS_COUNT = 2;
+    public static final String STATUS_ERR = "Status {%s} does not exist";
+    public static final String NOT_A_VALID_INTEGER = "Not a valid integer";
+
+    public static final String STATUS_CHANGE = "Status of task %s changed to %s";
+
+    protected ChangeStatusInFeedbackCommand(TaskRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    protected String executeCommand(List<String> parameters) {
+        ValidationHelper.validateArgumentsCount(parameters, EXPECTED_ARGUMENTS_COUNT);
+        int id = ParsingHelpers.tryParseInteger(parameters.get(0), NOT_A_VALID_INTEGER);
+        Feedback feedback = getRepository().findFeedbackById(id);
+        StatusFeedback status = ParsingHelpers.tryParseEnum(parameters.get(1), StatusFeedback.class, STATUS_ERR);
+
+        return changeStatus(feedback, status);
+    }
+
+    private String changeStatus(Feedback feedback, StatusFeedback status) {
+        feedback.setStatus(status);
+
+        return String.format(STATUS_CHANGE, feedback.getId(), status);
+
+    }
+
+
+}
